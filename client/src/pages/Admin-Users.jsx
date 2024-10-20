4ec4fcff5a835c0dc28bddf56f792b05ab7eb7bf
Chetan Belwal <chetan.belwal13@gmail.com>
2024-10-21 00:57:09 +0530

    delete user data ny id

--- client/src/pages/Admin-Users.jsx
+++ client/src/pages/Admin-Users.jsx
@@ -1,8 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "../store/auth";
 
 export const AdminUsers = () => {
   const { authorizationToken } = useAuth();
+  const [users, setUsers] = useState([]);
 
   const getAllUsersData = async () => {
     try {
@@ -14,15 +15,75 @@ export const AdminUsers = () => {
       });
 
       const data = await response.json();
+      setUsers(data);
       console.log("Users:", data);
     } catch (error) {
       console.log(error);
     }
   };
 
+//   Function which will Trigger when user will click on Delete Button
+const deleteUser = async(id)=>{
+    try {
+        const response = await fetch(`http://localhost:5000/api/admin/users/delete/${id}`, {
+          method: "DELETE",
+          headers: {
+            Authorization: authorizationToken,
+          },
+        });
+  
+        if (response.ok){
+            console.log("User deleted successfully")
+            getAllUsersData()
+        }
+      } catch (error) {
+        console.log(error);
+      }
+}
+
+
+
   useEffect(() => {
     getAllUsersData();
   }, []);
 
-  return <h1>Admin Users Page</h1>;
+  return (
+    <section className="admin-users-section">
+      <div className="container">
+        <h1>Admin Users Data</h1>
+      </div>
+      <div className="container admin-users">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Update</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users && users.length > 0 ? (
+              users.map((curUser, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{curUser.username}</td> 
+                    <td>{curUser.email}</td>
+                    <td>{curUser.phone}</td>
+                    <td>Edit</td>
+                    <td><button onClick={()=>deleteUser(curUser._id)}>Delete</button></td>
+                  </tr>
+                );
+              })
+            ) : (
+              <tr>
+                <td colSpan="5">No users found.</td> 
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
 };
--- server/controller/admin-controller.js
+++ server/controller/admin-controller.js
@@ -1,36 +1,46 @@
-const User = require("../models/user-model")
-const Contact = require("../models/contact-model")
-
-
+const User = require("../models/user-model");
+const Contact = require("../models/contact-model");
 
 //*********** * Get All users Data *  ***************
 
-const getAllUsers = async(req,res, next)=>{
-try {
+const getAllUsers = async (req, res, next) => {
+  try {
     //  getting user data from users model but we dont need password
-    const users = await User.find({},{password:0})
-    
-    if(!users ||users.length===0){
-        return res.status(404).json({message:"No user data available"})
+    const users = await User.find({}, { password: 0 });
+
+    if (!users || users.length === 0) {
+      return res.status(404).json({ message: "No user data available" });
     }
-    return res.status(200).json(users)
-} catch (error) {
-    next(error)
-}
-}
+    return res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+};
 
+//*********** * Delete All users Data *  ***************
 
+const deletUserById = async (req, res, next) => {
+  try {
+    //  deleting user data by users model here we get user id from url using req.params.id
+    const id = req.params.id;
+    await User.deleteOne({ _id: id });
+
+    return res.status(200).json({ message: "User Recor deleted Successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
 //*********** */ Get All Contact Data /*  ***************
-const getAllContacts = async(req,res, next)=>{
-   try {
-    const contacts = await Contact.find()
-    if(!contacts || contacts.length===0){
-        return res.status(404).json({message:"No Contact Data Available"})
+const getAllContacts = async (req, res, next) => {
+  try {
+    const contacts = await Contact.find();
+    if (!contacts || contacts.length === 0) {
+      return res.status(404).json({ message: "No Contact Data Available" });
     }
-    return res.status(200).json(contacts)
-   } catch (error) {
-    next(error)
-   }
-}
+    return res.status(200).json(contacts);
+  } catch (error) {
+    next(error);
+  }
+};
 
-module.exports = {getAllUsers, getAllContacts}
+module.exports = { getAllUsers, getAllContacts, deletUserById };
